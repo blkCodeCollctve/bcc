7820c9723ba8645a2c38a5d162d344c6b1374ae0
Zuri Hunter <zuri.hunter@thehumangeo.com>
2016-11-25 23:41:29 -0500

    Added smooth scrolling

--- js/main.js
+++ js/main.js
@@ -16,4 +16,18 @@ $(document).ready(function () {
             }
         });
     }
+
+    //Smooth Scrolling SOURCE => http://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+
 });
