5722b08860bc2328bd7e019d365ffe481809dbd6
Zuri Hunter <zuri.hunter@thehumangeo.com>
2016-11-25 23:31:50 -0500

    I don't like the about page color and i don't like the navbar......end of story

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log("Initializing Page"); 
+console.log("Initializing Page");
+//Added Navbar color change
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $(".bounce");
+    var offset = startchange.offset();
+
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $('.navbar').css('background-color', 'rgba(0,0,0,.50)');
+            }
+            else {
+                $('.navbar').css('background-color', 'transparent');
+            }
+        });
+    }
+});
