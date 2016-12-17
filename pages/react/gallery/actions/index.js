1788257561fd7bd4bc0959da8ce7137d3e8f013c
Zuri Hunter <zuri.hunter@thehumangeo.com>
2016-12-17 18:36:28 -0500

    Added gitignore to help with the node_modules stuff

---
+++ pages/react/gallery/actions/index.js
@@ -0,0 +1,1 @@
+console.log("I am the gallery page action reducer");
---
+++ pages/react/gallery/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        I am the Gallery Main App page.
+      </div>
+    );
+  }
+}
