9581f0d78c1853473feb36915791f65ccba8a541
zuri.hunter <zuri.hunter@thehumangeo.com>
2017-01-12 22:48:10 -0500

    Ended up moving navbar into a compoenet and now the routes work correctly, css needs to follow through

---
+++ src/components/navbar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render(){
+    return (
+      <div id="nav_bar" className="navbar">
+        <ul>
+          <li><a href="#sponsors">Sponsors</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#organizers">Organizers</a></li>
+            <li className="dropdown">
+              <a id="reflink" className="blockline">Activities</a>
+              <div className="dropborder">
+                <ul>
+                  <li className="dropdown-content"><a href="/pages/volunteer.html">Volunteer</a></li>
+                  <li className="dropdown-content"><a href="/pages/projects.html">Projects</a></li>
+                  <li className="dropdown-content"><a href="/pages/gallery.html">Gallery</a></li>
+                  <li className="dropdown-content"><a href="/pages/talks-blogs.html">Blogs & Talks</a></li>
+                </ul>
+              </div>
+            </li>
+          <li><a href="#event">Events</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+      </div>
+    )
+  }
+}
