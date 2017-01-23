d74a560e52b3f68532e5248da6aed448a4eb7008
zuri.hunter <zuri.hunter@thehumangeo.com>
2017-01-22 20:05:59 -0500

    Battling css

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,6 +5,8 @@ import { fetchProjects } from '../actions/index'
 
 class Projects extends Component {
 
+//Stats on code history RECENT ACTIVITY https://developer.github.com/v3/repos/statistics/
+
   componentWillMount(){
     this.props.fetchProjects()
   }
@@ -17,25 +19,71 @@ renderProjects(){
   } else {
     return this.props.projects.map((project)=> {
       return (
-        <li className="list-group-item" key={project.id}>
-          {project.forks_url}
-        </li>
+        <div className="project-padding">
+          <div className="project">
+            <div className="project-name">
+              <h1>{project.name}</h1>
+              <hr/>
+            </div>
+
+            <p className="list-group-item" key={project.id}>
+              Description: {project.description}<br/>
+              Language: {project.languages_url}<br/>
+              Contributors: {project.contributors_url}<br/>
+              Open Issues: {project.open_issues_count}<br/>
+            </p>
+          </div>
+        </div>
       )
     })
   }
+}
+
+renderFilters (){
+  return (
+    <div className="select-container">
+
+      <div className="sortby">
+        <label>Sort By:</label>
+        <select>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+        </select>
+      </div>
+
+      <div className="filterby">
+        <label>Filter By:</label>
+        <select>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="mercedes">Mercedes</option>
+          <option value="audi">Audi</option>
+        </select>
+      </div>
+    </div>
+  )
 }
 
 
   render() {
     return (
       <div>
-        <div className="row">
-          <div className="col-md-2">
+        <div className="project-bg"></div>
+        <div className="row project-container">
+          <div className="col-md-1">
           </div>
-          <div className="col-md-8">
+          <div className="col-md-10">
+            <div className="project-headline">
+              Projects
+            </div>
+              {this.renderFilters()}
+            <div className="fixed-project-container">
             {this.renderProjects()}
+            </div>
           </div>
-          <div className="col-md-2">
+          <div className="col-md-1">
           </div>
         </div>
       </div>
